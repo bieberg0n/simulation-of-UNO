9a44bb0f98b2083f4df6be1ddfd2c9729dc8c7a4
bj <biebergong@gmail.com>
2019-08-30 11:49:37 +0800

    update

--- simulation.py
+++ simulation.py
@@ -9,7 +9,7 @@ from time import sleep
 
 
 def log(*args):
-	print(*args)
+	print(*args, '\n')
 
 
 def color_update():
@@ -48,100 +48,68 @@ class Uno:
 			if card[-2:] not in ('转色', '+4', '跳过', '反转', '+2'):
 				break
 
-		# card_selected = self.user_a[num]
-		# state = []
 		state = [card[0], card[1:], 0]
-		# state.append(0)
 		self.user_a.pop(num)
 		log('玩家 A 打出了手中的 {} , TA 手中还剩下 {} 张牌。'.format(card, len(self.user_a)))
-		log()
 		return state
 
 	def decision_making(self, current_user):
-		card_selected_2 = []
+		card_selected = ''
 		if self.state[2] > 0:
-			if len(current_user) == 1:
-				for n in range(0, self.state[2]):
+			if len(current_user) > 1:
+				for card in current_user:
+					if card[-2:] == '+4' or (card[-2:] in ('反转', '+2') and (self.state[1] == card[-2:] or card[0] == self.state[0])):
+						color, v, v2 = card[0], card[-2:], self.state[2]
+						if card[-2:] == '+2':
+							v2 += 2
+						elif card[-2:] == '+4':
+							color = color_update()
+							v = '无'
+							v2 += 4
+
+						self.state = [color, v, v2]
+						current_user.remove(card)
+						card_selected = card
+						break
+
+			if not card_selected:
+				for n in range(self.state[2]):
 					current_user.append(self.deliver_cards())
 				self.state[2] = 0
+
+		else:
+			for card in current_user:
+				if card[-2:] in ('+4', '转色') or card[0] == self.state[0] or card[1:] == self.state[1]:
+					color, v, v2 = card[0], card[1:], self.state[2]
+					if card[-2:] == '+4':
+						color, v, v2 = color_update(), '无', v2 + 4
+					elif card[-2:] == '+2':
+						v2 += 2
+					elif card[-2:] == '转色':
+						color = color_update()
+
+					self.state = [color, v, v2]
+					current_user.remove(card)
+					card_selected = card
+					break
+
 			else:
-				for e in current_user:
-					if e[-2:] == '+4':
-						card_selected_2 = e					
-						self.state[0] = color_update()
-						self.state[1] = '无'
-						self.state[2] += 4
-						current_user.remove(e)
-						break
-					elif e[-2:] == '反转':
-						if self.state[1] == '反转':
-							card_selected_2 = e
-							self.state[0] = e[0]
-							current_user.remove(e)
-							break
-						elif e[0] == self.state[0]:
-							card_selected_2 = e
-							self.state[1] = '反转'
-							current_user.remove(e)
-							break
-					elif e[-2:] == '+2':
-						if self.state[1] == '+2':
-							card_selected_2 = e
-							self.state[0] = e[0]
-							self.state[2] += 2
-							current_user.remove(e)
-							break
-						elif e[0] == self.state[0]:
-							card_selected_2 = e
-							self.state[1] = '+2'
-							self.state[2] += 2
-							current_user.remove(e)
-							break
-				if card_selected_2 == []:
-					for n in range(0, self.state[2]):
-						current_user.append(self.deliver_cards())
-						self.state[2] = 0
-		else:
-			for e in current_user:
-				if e[-2:] == '+4':
-					card_selected_2 = e					
-					self.state[0] = color_update()
-					self.state[1] = '无'
-					self.state[2] += 4
-					current_user.remove(e)
-					break
-				elif e[-2:] == '转色':
-					card_selected_2 = e					
-					self.state[0] = color_update()
-					current_user.remove(e)
-					break
-				else:
-					if e[0] == self.state[0]:
-						card_selected_2 = e
-						self.state[1] = e[1:]
-						current_user.remove(e)
-						break
-					elif e[1:] == self.state[1]:
-						card_selected_2 = e
-						self.state[0] = e[0]
-						current_user.remove(e)
-						break
-			if card_selected_2 == []:
 				current_user.append(self.deliver_cards())
-		if card_selected_2 == []:
-			print('当前玩家无牌可打，因此必须摸牌。TA 手中还剩下 ' + str(len(current_user)) + ' 张牌。\n')
-			print('现在的牌桌颜色是 ' + self.state[0] + ' , 牌桌符号是 ' + self.state[1] + ' , 下家要罚 ' + str(self.state[2]) + ' 张牌。\n')
+
+		if card_selected:
+			log('当前玩家打出了手中的 {} , TA 手中还剩下 {} 张牌。'.format(card_selected, len(current_user)))
+			log('现在的牌桌颜色是 {} , 牌桌符号是 {} , 下家要罚 {} 张牌。'.format(self.state[0], self.state[1], self.state[2]))
 		else:
-			print('当前玩家打出了手中的 ' + card_selected_2 + ' , TA 手中还剩下 ' + str(len(current_user)) + ' 张牌。\n')
-			print('现在的牌桌颜色是 ' + self.state[0] + ' , 牌桌符号是 ' + self.state[1] + ' , 下家要罚 ' + str(self.state[2]) + ' 张牌。\n')
-		end_condition = len(current_user)
-		return end_condition
+			log('当前玩家无牌可打，因此必须摸牌。TA 手中还剩下 {} 张牌。'.format(len(current_user)))
+			log('现在的牌桌颜色是 {} , 牌桌符号是 {} , 下家要罚 {} 张牌。'.format(self.state[0], self.state[1], self.state[2]))
+
+		return len(current_user)
 
 	def run(self):
 		# 先生成出一副牌
 		self.generate_cards()
-		log('正在载入。\n')
-		log('牌组已经生成。\n')
+		log('正在载入。')
+		log('牌组已经生成。')
 		sleep(1)
 
 		# 然后是开局发牌
@@ -155,12 +123,12 @@ class Uno:
 			for u, _ in users:
 				u.append(self.deliver_cards())
 
-		log('发牌已经完成。\n')
-		log('现在由 A 开始出第一张牌。\n')
+		log('发牌已经完成。')
+		log('现在由 A 开始出第一张牌。')
 		# 由 A 出第一张牌
 		self.state = self.start()
-		log('现在的牌桌颜色是 {} , 牌桌符号是 {} , 下家要罚 {}  张牌。\n'.format(self.state[0], self.state[1], self.state[2]))
-		log('现在轮到 B 出牌。\n')
+		log('现在的牌桌颜色是 {} , 牌桌符号是 {} , 下家要罚 {}  张牌。'.format(self.state[0], self.state[1], self.state[2]))
+		# log('现在轮到 B 出牌。')
 		sleep(1)
 
 		# 然后是游戏轮局、卡牌结算、 UNO 呼喊、 游戏结束。
@@ -170,10 +138,12 @@ class Uno:
 			# 用 while 来保证除非终局，游戏将永远持续下去
 			# 先让 B 出牌。
 			for user, name in users:
+				log('现在轮到 ' + name + ' 出牌。')
 				if self.decision_making(user) == 0:
-					log('玩家 ' + name + ' 已经赢得了比赛！\n')
+					log('玩家 ' + name + ' 已经赢得了比赛！')
 					return
-				sleep(1)
+				else:
+					sleep(1)
 
 
 def main():
